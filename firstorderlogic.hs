f8b6fa8df0626a290e956ed55c291b948093c924
Matthe Owusu <mo17326@my.bristol.ac.uk>
2019-03-03 10:53:52 +0000

    starting to add predicated statements

--- firstorderlogic.hs
+++ firstorderlogic.hs
@@ -1,9 +1,12 @@
 import Prelude hiding (Num)
 import qualified Prelude (Num)
 
+
+
 type Variable = String
 type State = Variable -> Boolean
-type Predicate = Variable -> Bool
+type Predicate = Integer -> Maybe Bool
+
 
 data Quantified = U Variable Quantified | E Variable Quantified | B Boolean
 
@@ -11,7 +14,12 @@ data Quantified = U Variable Quantified | E Variable Quantified | B Boolean
 data Boolean = Var Variable | Terminal Bool | Boolean :=> Boolean
  | Boolean :^ Boolean | Not Boolean
 
-
+ -- test things
+simple_bool = (Terminal True) :=> (Terminal True)
+example_quantified = (U "x" (B (Var "x")))
+s :: State
+s "undefined" = Terminal False
+ --  end of test things
 
 update :: State -> Boolean -> Variable -> State
 update st a v x
@@ -27,19 +35,22 @@ eval_boolean (b1 :^ b2) s =  (eval_boolean b1 s) && (eval_boolean b2 s)
 eval_boolean (Not b1) s =  not (eval_boolean b1 s)
 
 
--- evaluate a boolean expression with quantifiers
+-- evaluate a boolean expression with quantifiers over boolean expressions (ie True/False)
 eval_quantified :: Quantified -> State -> Bool
 eval_quantified (U v q) s = (eval_quantified q (update s (Terminal True) v)) && (eval_quantified q (update s (Terminal False) v))
 eval_quantified (E v q) s = (eval_quantified q (update s (Terminal True) v)) || (eval_quantified q (update s (Terminal False) v))
 eval_quantified (B b) s = eval_boolean b s
 
--- test things
-simple_bool = (Terminal True) :=> (Terminal True)
-example_quantified = (U "x" (B (Var "x")))
-s :: State
-s "x" = Terminal True
-s "y" = Terminal False
+eval_1 :: Quantified -> Bool
+eval_1 q = eval_quantified q s
 
+
+expression =  (E "x" (U "y" (B (      (Not (Var "x")) :=> (Var "y")        ))))
+expression_2 = (E "x" (U "y" (B(      (Not (Var "x")) :=> (Terminal False)       ))))
+
+--exampe preedicate
+p :: Predicate
+p 1 = Just False
+p 2 = Just True
 
 -- there exists a student (x) such that, if they fail the exam, all other students (y) will also fail
-expression = (E "x" (U "y" (B ((Not (Var "x")) :=> (Not (Var "y"))))))
