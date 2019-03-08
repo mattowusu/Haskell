fbf90ff17f351ecfddea433568067c4d7d9e2a82
Matthe Owusu <mo17326@my.bristol.ac.uk>
2019-03-08 12:16:17 +0000

    multiple tyeps of bracket

--- brackets.hs
+++ brackets.hs
@@ -1,12 +1,15 @@
-expr1 = "3([241(1]12))"
-expr2= "(fd[fg(21)]1)"
+expr1 = "3([241(1]12))" -- ([(]))
+expr2= "(fd[fg(21)]1)" -- ([()])
 
 
-match :: String -> Int -> Bool
-match [] 0 = True
+match :: String -> String -> Bool
+match [] [] = True
 match [] _ = False
-match (x:xs) n
-  | n < 0 = False
-  | x=='(' = match xs (n+1)
-  | x==')' = match xs (n-1)
-  | otherwise = match xs n
+match (')':xs) (y:ys) = and [(y==')'), match xs ys]
+match (']':xs) (y:ys) = and [(y==']'), match xs ys]
+match ('}':xs) (y:ys) = and [(y=='}'), match xs ys]
+match (x:xs) ys
+  |( x=='(') = match xs (')':ys)
+  | (x=='[') = match xs (']':ys)
+  | (x=='{') = match xs ('}':ys)
+  | otherwise = match xs ys
